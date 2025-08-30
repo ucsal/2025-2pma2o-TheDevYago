8da3d0f56c67259d9459a1ea6542820e4660b5ae
TheDevYago <yagocpm2020@gmail.com>
2025-08-30 13:49:29 -0300

    Refactor: uso correto do principio de OCP
    Uso de Lambdas

--- src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
+++ src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
@@ -1,11 +1,17 @@
 package br.com.mariojp.solid.ocp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DiscountCalculator {
+    private final Map<CustomerType, PolicyDiscount> policies = new HashMap<>();
+    public DiscountCalculator (){
+        policies.put(CustomerType.REGULAR, amount -> amount * 0.95);
+        policies.put(CustomerType.PREMIUM, amount -> amount * 0.90);
+        policies.put(CustomerType.PARTNER, amount -> amount * 0.88);
+    }
     public double apply(double amount, CustomerType type){
-        switch (type){
-            case REGULAR: return amount * 0.95;
-            case PREMIUM: return amount * 0.90;
-            default: return amount;
-        }
+        PolicyDiscount policy = policies.getOrDefault(type, a -> a);
+        return policy.apply(amount);
     }
 }
